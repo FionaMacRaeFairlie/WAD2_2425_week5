49929a16d6cbd899e8bd56b6810fb583d4d3980f
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2025-02-21 09:12:52 +0000

    remove old example from site

--- example1.js
+++ example1.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use((req, res, next) => {
-  console.log('\n ALWAYS')
+  console.log('\n\nALWAYS')
   next()
 })
 
@@ -12,6 +12,41 @@ app.get('/a', (req, res) => {
 })
 app.get('/a', (req, res) => {
   console.log('/a: never called');
+})
+app.get('/b', (req, res, next) => {
+  console.log('/b: route not terminated')
+  next()
+})
+app.use((req, res, next) => {
+  console.log('SOMETIMES')
+  next()
+})
+app.get('/b', (req, res, next) => {
+  console.log('/b (part 2): error thrown' )
+  throw new Error('b failed')
+})
+app.use('/b', (err, req, res, next) => {
+  console.log('/b error detected and passed on')
+  next(err)
+})
+
+app.get('/c', (err, req) => {
+  console.log('/c: error thrown')
+  throw new Error('c failed')
+})
+app.use('/c', (err, req, res, next) => {
+  console.log('/c: error detected but not passed on')
+  next()
+})
+
+app.use((err, req, res, next) => {
+  console.log('unhandled error detected: ' + err.message)
+  res.send('500 - server error')
+})
+
+app.use((req, res) => {
+  console.log('route not handled')
+  res.send('404 - not found')
 })
 
 const port = process.env.PORT || 3000
